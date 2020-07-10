506c0eca4ea93691978ab4c140bc57c2b70f7354
Kenny K Yeo <kenny@yeoyou.net>
2020-07-11 00:46:58 +1000

    try to use async/await

--- thumbnail-worker.js
+++ thumbnail-worker.js
@@ -5,72 +5,68 @@ const appendBuffer = (buffer1, buffer2) => {
   return tmp;
 };
 
-const fetchEXIFThumb = (url) =>
-  fetch(url)
-    .then((response) => {
-      const reader = response.body.getReader();
-      let start;
-      let end;
-      let buffer;
-      return new ReadableStream({
-        start(controller) {
-          function pump() {
-            return reader.read().then(({ done, value }) => {
-              console.log(url, 'value', value.length);
-              buffer = buffer
-                ? appendBuffer(buffer, value)
-                : new Uint8Array(value);
-              for (let i = 2, j = buffer.length; i < j; i += 1) {
-                if (buffer[i] !== 0xff) continue; // eslint-disable-line
-                if (!start && buffer[i + 1] === 0xd8) {
-                  start = i;
-                  continue; // eslint-disable-line
-                }
-                if (start && buffer[i + 1] === 0xd9) end = i + 2;
-              }
-
-              if (start && end) {
-                const stream = buffer.subarray(start, end);
-                controller.enqueue(stream);
-                controller.close();
-                reader.cancel('success');
-                return;
-              }
-
-              if (done) {
-                controller.close();
-                return;
-              }
-
-              // stop streaming
-              if (buffer.length > 80000) {
-                controller.close();
-                reader.cancel('cannot find thumbail');
-                return;
-              }
-
-              controller.enqueue(value);
-              return pump(); // eslint-disable-line
-            });
+const fetchEXIFThumb = async (url) => {
+  const response = await fetch(url);
+  const reader = response.body.getReader();
+  let start;
+  let end;
+  let buffer;
+  const stream = new ReadableStream({
+    start(controller) {
+      async function pump() {
+        const { done, value } = await reader.read();
+        console.log(url, 'value', value.length);
+        buffer = buffer
+          ? appendBuffer(buffer, value)
+          : new Uint8Array(value);
+        for (let i = 2, j = buffer.length; i < j; i += 1) {
+          if (buffer[i] !== 0xff) continue; // eslint-disable-line
+          if (!start && buffer[i + 1] === 0xd8) {
+            start = i;
+            continue; // eslint-disable-line
           }
-          return pump();
-        },
-        cancel(reason) {
-          throw reason;
-        },
-      });
-    })
-    .then((stream) => new Response(stream))
-    .then((response) => response.blob());
+          if (start && buffer[i + 1] === 0xd9) end = i + 2;
+        }
 
-onmessage = (e) => {
+        if (start && end) {
+          const stream = buffer.subarray(start, end);
+          controller.enqueue(stream);
+          controller.close();
+          reader.cancel('success');
+          return;
+        }
+
+        if (done) {
+          controller.close();
+          return;
+        }
+
+        // stop streaming
+        if (buffer.length > 80000) {
+          controller.close();
+          reader.cancel('cannot find thumbail');
+          return;
+        }
+
+        controller.enqueue(value);
+        return pump(); // eslint-disable-line
+      }
+      return pump();
+    },
+    cancel(reason) {
+      throw reason;
+    },
+  });
+  return new Response(stream).blob();
+}
+
+onmessage = async (e) => {
   const { path } = e.data;
-  fetchEXIFThumb(path)
-    .then(blob => {
-      // console.log("path", path, "url", url);
-      postMessage({ blob });
-    })
-    .catch((e) => {
+  try {
+    const blob = await fetchEXIFThumb(path);
+    // console.log("path", path, "url", url);
+    postMessage({ blob });
+  } catch (e) {
       console.info('Error: ', e.message, path);
-    });
+  }
 };
